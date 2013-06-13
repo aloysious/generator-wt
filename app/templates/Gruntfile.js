a0e27f902bb2946dcb53ded9bec08c2769f43d43
aloysiousliang <aloysiousliang@gmail.com>
2013-06-14 00:16:07 +0800

    change template

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
         // name作为gallery发布后的模块名
         // version是版本，也是发布目录
         // author必须是{name: "xxx", email: "xxx"}格式
-        //pkg: grunt.file.readJSON('abc.json'),
+        pkg: grunt.file.readJSON('package.json'),
         //banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         //    '<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>\n' +
         //    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
@@ -35,49 +35,78 @@ module.exports = function(grunt) {
 				]
 			}
 		},
+		
+		// Configuration to be run (and then tested).
+		wtbuild: {
+			compile: {
+				options: {
+					compileOnly: true,
+					domain: '<%= pkg.domain%>',
+					group: '<%= pkg.group%>',
+					project: '<%= pkg.project%>',
+					version: '<%= pkg.version%>'
+				},
+				files: [{
+					src: 'src/page/*.{shtml,html}',
+					dest: 'build/page'
+				}]
+			},
+			dep: {
+				options: {
+					packages: [{
+						name: 'widget',
+						path: 'src/'
+					}, {
+						name: 'util',
+						path: 'src/'
+					}],
+					depExt: '.dep',
+					depOnly: true,
+					depFilePath: 'src/mods.js'
+				},
+				files: [{
+					src: 'src/**/*.js'
+				}]
+			}
+		},
 
-        // kmc打包任务，默认情况，入口文件是index.js，可以自行添加入口文件，在files下面
-        // 添加
-        kmc: {
-            options: {
-                packages: [
-                    {
-                        name: '<%= pkg.name %>',
-                        path: '../'
-                    }
-                ],
-                map: [["<%= pkg.name %>/", "gallery/<%= pkg.name %>/"]]
-            },
-            main: {
-                files: [
-                    {
-                        src: "<%= pkg.version %>/index.js",
-                        dest: "<%= pkg.version %>/build/index.js"
-                    }
-                ]
-            }
-        },
         // 打包后压缩文件
         // 压缩文件和入口文件一一对应
         uglify: {
-            options: {
-                //banner: '<%= banner %>',
-                beautify: {
-                    ascii_only: true
-                }
-            },
-            base: {
-                files: {
-                    '<%= pkg.version %>/build/index-min.js': ['<%= pkg.version %>/build/index.js']
-                }
-            }
-        }
+			dynamic_mappings: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/',
+						src: ['**/*.js'],
+						dest: 'build/',
+						ext: '-min.js'
+					}
+				]
+			}
+        },
+
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/',
+						src: ['**/*.{js,css}'], 
+						dest: 'build/'
+					}
+				]
+			}
+		}
     });
 
     // 使用到的任务，可以增加其他任务
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-kmc');
-    return grunt.registerTask('default', ['kmc', 'uglify', 'jshint', 'less']);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-wtbuild');
+    grunt.registerTask('buildmods', ['wtbuild:dep']);
+    grunt.registerTask('default', ['jshint', 'less', 'wtbuild:dep', 'wtbuild:compile', 'uglify', 'copy']);
+    grunt.registerTask('build', ['jshint', 'less', 'wtbuild:dep', 'wtbuild:compile', 'uglify', 'copy']);
 };
